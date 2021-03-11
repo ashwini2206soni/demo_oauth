97d18313d09ecc67159e1592146025abec94f25e
ashwinisoni2206 <ashwini19soni@gmail.com>
2021-03-11 03:13:03 -0500

    added new background color

---
+++ src/components/LogoutHooks.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useGoogleLogout } from 'react-google-login';
+
+const clientId =
+  '832040387372-j1ij4vn25qhtrmm9c2n4dtqdjtd4nr2i.apps.googleusercontent.com';
+
+function LogoutHooks() {
+  const onLogoutSuccess = (res) => {
+    console.log('Logged out Success');
+    alert('Logged out Successfully ✌');
+  };
+
+  const onFailure = () => {
+    console.log('Handle failure cases');
+  };
+
+  const { signOut } = useGoogleLogout({
+    clientId,
+    onLogoutSuccess,
+    onFailure,
+  });
+
+  return (
+    <button onClick={signOut} className="button">
+      <img src="icons/google.svg" alt="google login" className="icon"></img>
+
+      <span className="buttonText">Sign out</span>
+    </button>
+  );
+}
+
+export default LogoutHooks;
